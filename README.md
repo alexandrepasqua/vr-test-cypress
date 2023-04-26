6fe70fa0b7ec6594ab8583ef1ce3b0154f1fcb32
alexandrepasqua <alexandre_pasqua@hotmail.com>
2023-04-26 10:07:41 -0300

    Readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# PASSO A PASSO
+
+**1 - Passo de entre na pasta do projeto**
+```
+cypress-framework
+```
+
+**2- Para rodar o Cypress, execute o comando:**
+
+```
+npm run cypress:open
+```
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,19 @@
 describe('vr-site-map', () => {
   it('passes', () => {
-    cy.visit('https://www.vr.com.br') //Visitar a pagina
-    cy.get('#lum_4028808471FC1D5D0171FE80340D6AC8_quickAcceptTerm').click() //Fazer o aceite dos termos
-    cy.get(':nth-child(3) > .vr-quick-navigation__link').click() //Clicar no link do "Prá você"
-    cy.get('.vr-hero__actions > .vr-button--negative').click() //Clicar no botão "Onde usar meu cartão VR"
-    cy.get('.vr-container__mapa').should('be.visible') //Verificar se o mapa esta visivel
+    //Visitar a pagina da VR
+    cy.visit('https://www.vr.com.br') 
+    
+    //Fazer o aceite dos termos
+    cy.get('#lum_4028808471FC1D5D0171FE80340D6AC8_quickAcceptTerm').click() 
+
+    //Clicar no link do "Prá você"
+    cy.get(':nth-child(3) > .vr-quick-navigation__link').click() 
+
+    //Clicar no botão "Onde usar meu cartão VR"
+    cy.get('.vr-hero__actions > .vr-button--negative').click() 
+
+    //Verificar se o mapa esta visivel
+    cy.get('.vr-container__mapa').should('be.visible') 
+
   })
 })
